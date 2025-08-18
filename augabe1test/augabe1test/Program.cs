f6982e973fced246ad1e442615b9839887141796
1sarka <sarel.mid@gmail.com>
2025-08-18 14:50:31 +0200

    Add project files.

---
+++ augabe1test/augabe1test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace augabe1test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
